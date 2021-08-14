6b0d9fd5bd9fd87f3c14af8dff643924abb59225
masonkimseoul <87306418+masonkimseoul@users.noreply.github.com>
2021-08-15 02:48:02 +0900

    week_04

--- week_04/2504.py
+++ week_04/2504.py
@@ -16,83 +16,33 @@ class Stack:
 
 par_array=list(input())
 s=Stack()
-calc=Stack
 answer=0
-tmp=0
+tmp=1
 flag=0
 for i in par_array:
-    if s.isEmpty():
-        if i==')' or i==']':
-            print(0)
-            break
+    if i=='(':
         s.push(i)
-    else:
-        if i=='(' or i=='[':
-            s.push(i)
-        elif i==')':
-            if s.peek()=='(':
-                s.pop()
-
-
-    '''
-    if s.isEmpty():
-        if i==')' or i==']':
-            print(0)
-            break
+        tmp*=2
+    elif i=='[':
         s.push(i)
-    else:
-        if i=='(' or i=='[':
-            s.push(i)
-            if tmp!=0:
-                answer+=tmp
-                tmp=0
-        elif i==')':
-            if s.peek()=='(':
-                s.pop()
-                if flag==0:
-                    if s.isEmpty():
-                        if tmp==0:
-                            tmp+=2
-                            answer+=tmp
-                        else:
-                            answer+=tmp
-                            answer*=2
-                        tmp=0
-                        flag=1
-                    elif tmp==0:
-                        tmp+=2
-                    else:
-                        tmp*=2
-                else:
-                    if s.isEmpty():
-                        if tmp==0:
-                            tmp+=2
-                        else:
-                            tmp*=2
-                        answer+=tmp
-                    elif tmp==0:
-                        tmp+=2
-                    else:
-                        tmp*=2
-
-            elif s.peek()=='[':
-                print(0)
-                break
-        elif i==']':
-            if s.peek()=='[':
-                s.pop()
-                if s.isEmpty():
-                    if tmp == 0:
-                        tmp += 3
-                    answer += tmp
-                    tmp = 0
-                elif tmp==0:
-                    tmp+=3
-                else:
-                    tmp*=3
-            elif s.peek()=='(':
-                print(0)
-                break
-    print(answer, tmp, flag)
+        tmp*=3
+    elif i==')':
+        if len(s.top)==0 or s.peek()=='[':
+            answer=0
+            break
+        if flag=='(':
+            answer+=tmp
+        tmp//=2
+        s.pop()
+    elif i==']':
+        if len(s.top)==0 or s.peek()=='(':
+            answer=0
+            break
+        if flag=='[':
+            answer+=tmp
+        tmp//=3
+        s.pop()
+    flag=i
+if not s.isEmpty():
+    answer=0
 print(answer)
-    '''
