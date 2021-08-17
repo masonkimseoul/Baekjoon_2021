f187eb1c6d61eb73d38726a893d50aeabaebb727
masonkimseoul <87306418+masonkimseoul@users.noreply.github.com>
2021-08-17 11:28:14 +0900

    week_06
    changed logic(permutation to combinations)

--- week_06/2798.py
+++ week_06/2798.py
@@ -1,8 +1,8 @@
-from itertools import permutations
+from itertools import combinations
 N,M=map(int,input().split())
 card_val=list(map(int,input().split()))
 
-available_set=set(permutations(card_val,3))
+available_set=set(combinations(card_val,3))
 answer=0
 for i in available_set:
     if sum(i)>M:
